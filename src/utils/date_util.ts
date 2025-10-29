3ccd5491d4f4a237a139055920c5c8d761778b4b
Rohit Kumar Singh <rohitkumar@infoservices.com>
2025-10-29 22:21:06 +0530

    handled invalid date case

--- src/utils/date_util.ts
+++ src/utils/date_util.ts
@@ -1,10 +1,14 @@
 import { format, parse, differenceInYears } from "date-fns";
 
 export const getAgeFromDate = (birthDate: string): number => {
-  const today = format(new Date(), "MM/yyyy");
-  const d = parse(birthDate, "MM/yyyy", new Date());
-  const t = format(d, "MM/yyyy");
-  const parsedDate1 = parse(today, "MM/yyyy", new Date());
-  const parsedDate2 = parse(t, "MM/yyyy", new Date());
-  return differenceInYears(parsedDate1, parsedDate2) + 1;
+  try {
+    const today = format(new Date(), "MM/yyyy");
+    const d = parse(birthDate, "MM/yyyy", new Date());
+    const t = format(d, "MM/yyyy");
+    const parsedDate1 = parse(today, "MM/yyyy", new Date());
+    const parsedDate2 = parse(t, "MM/yyyy", new Date());
+    return differenceInYears(parsedDate1, parsedDate2) + 1;
+  } catch (error) {
+    return 0;
+  }
 };
